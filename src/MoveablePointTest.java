6186f50031788b3b6bbc45802d2a5a6d1a4ffde9
= <=>
2019-07-09 14:45:27 +0700

    huhu

---
+++ src/MoveablePointTest.java
@@ -0,0 +1,6 @@
+public class MoveablePointTest {
+    public static void main(String[] args) {
+        MoveablePoint moveablePoint = new MoveablePoint(2.0f, 4.0f, 5.0f, 5.0f);
+        System.out.println(moveablePoint.move());
+    }
+}
---
+++ src/PointTest.java
@@ -0,0 +1,7 @@
+public class PointTest {
+    public static void main(String[] args) {
+        Point point = new Point(2.0f,4.0f);
+        String chuoi = point.toString();
+        System.out.println(chuoi);
+    }
+}
